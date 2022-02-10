6b497b509425ab8ce1dcc101d535aa79dfcac5c5
Icy Denev <icy.denev@outlook.com>
2022-02-11 00:02:31 +0200

    icyDenev's algorithm for finding the best word (a modified version of eto's one)

--- Source/Program.cs
+++ Source/Program.cs
@@ -80,29 +80,77 @@ namespace Wordler
 
         private static void CalculateProbabilities()
         {
-            int letterListCount = Solver.wordList.Count * 5;
+            int letterListCount = Solver.wordList.Count;
             var wordlist = Solver.wordList;
-            Dictionary<char, double> letterProbability = new Dictionary<char, double>();
-            for (char i = 'a'; i <= 'z'; ++i)
+            Dictionary<char, double>[] letterProbability = new Dictionary<char, double>[5];
+
+            for (int i = 0; i < 5; ++i)
             {
-                int occurances = 0;
-                foreach (var word in wordlist)
-                {
-                    occurances += word.Count(e => e == i);
-                }
-                letterProbability.Add(i, (double)occurances / (double)letterListCount);
+                letterProbability[i] = new Dictionary<char, double>();
             }
-            letterProbability = letterProbability.OrderByDescending(e => e.Value).ToDictionary(e => e.Key, e => e.Value);
+
+            #region eto1302's way of finding best words
+            //for (char i = 'a'; i <= 'z'; ++i)
+            //{
+            //    int occurances = 0;
+            //    foreach (var word in wordlist)
+            //    {
+            //        occurances += word.Count(e => e == i);
+            //    }
+            //    letterProbability.Add(i, (double)occurances / (double)letterListCount);
+            //}
+            //letterProbability = letterProbability.OrderByDescending(e => e.Value).ToDictionary(e => e.Key, e => e.Value);
+            //Dictionary<string, double> wordProbability = new Dictionary<string, double>();
+            //foreach (var word in wordlist)
+            //{
+            //    double probability = 0;
+            //    foreach (var letter in string.Join("", word.ToCharArray().Distinct()))
+            //    {
+            //        probability += (letterProbability[letter] / Math.Log(letterProbability[letter]));
+            //    }
+            //    wordProbability.Add(word, -probability);
+            //}
+            #endregion
+
+            #region icyDenev's way of finding best words
+            //it's a modified version of eto1302's idea of finding the best words
+            //but rather than calculating the general probability of the letters
+            //this algorithm calculates the probability of a letter in a specific position and based on that formulates the probability of the words
+            for (int i = 0; i < 5; ++i)
+            {
+                for (char j = 'a'; j <= 'z'; ++j)
+                {
+                    int occurances = 0;
+
+                    foreach (var word in wordlist)
+                    {
+                        occurances += word[i] == j ? 1 : 0;
+                    }
+
+                    letterProbability[i].Add(j, (double)occurances / (double)letterListCount);
+                }
+            }
+            
+
+            letterProbability = letterProbability.Select(e => e.OrderByDescending(e => e.Value).ToDictionary(e => e.Key, e => e.Value)).ToArray();
             Dictionary<string, double> wordProbability = new Dictionary<string, double>();
+
             foreach (var word in wordlist)
             {
                 double probability = 0;
+                int index = 0;
+
                 foreach (var letter in string.Join("", word.ToCharArray().Distinct()))
                 {
-                    probability += (letterProbability[letter] / Math.Log(letterProbability[letter]));
+                    probability += (letterProbability[index][letter] / Math.Log(letterProbability[index][letter]));
+
+                    ++index;
                 }
+
                 wordProbability.Add(word, -probability);
             }
+            #endregion
+
             wordProbability = wordProbability.OrderByDescending(e => e.Value).ToDictionary(e => e.Key, e => e.Value);
             File.WriteAllText(@"..\..\..\wordProbability.txt", JsonSerializer.Serialize(wordProbability));
         }
